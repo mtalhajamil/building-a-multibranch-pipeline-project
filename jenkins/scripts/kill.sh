ed0a2e218dc5294fe87ee54ae08f7bc5a28b9544
mtalhajamil <talha.jamil@venturedive.com>
2018-10-16 11:31:07 +0500

    updated kill script

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -4,5 +4,5 @@ echo 'The following command terminates the "npm start" process using its PID'
 echo '(written to ".pidfile"), all of which were conducted when either'
 echo '"deliver-for-development.sh" or "deliver-for-development.sh" was executed.'
 set -x
-if pgrep $(cat .pidfile); then kill $(cat .pidfile); fi
+echo if pgrep $(cat .pidfile); then kill $(cat .pidfile); fi
 # kill $(cat .pidfile)
